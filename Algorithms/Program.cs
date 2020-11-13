9905d477378f9f8b3178e808805d2b94bbf35178
Batuhan Karaduman <batuhankaraduman@gmail.com>
2020-11-13 16:05:48 +0300

    comment out

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -13,8 +13,8 @@ namespace Algorithms
         {
             double[] unsortedList = { 36, 2, 28, 1, 23, 7, 44 };
 
-            // BubbleSortAlgorithm.BubbleSort(unsortedList);
-
+            BubbleSortAlgorithm.BubbleSort(unsortedList);
+            
             SelectionSortAlgorithm.SelectionSort(unsortedList);
         }
     }
